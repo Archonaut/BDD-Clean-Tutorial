a02d9a7aa966d35df3921201480ecdb7865c6c21
Archonaut <diskursmarker@gmail.com>
2020-11-30 11:27:50 +0100

    cleaned up the code

--- cucumber_automation/features/login.feature
+++ cucumber_automation/features/login.feature
@@ -5,27 +5,9 @@ Feature: Login successfully
     Successfully log in, starting from the Main Page
 
     Scenario: Not already logged in
-    #Given the user is on the landing page
-    Given I am on the main page
-    #When I click on the button for the Login-Page               
-    #Then I get to the Login-Page                                
-    When I scroll down                                          
-    #And I fill in my username                                   
-    #And I type in the correct password and confirm via Enter    
-    Then I am greeted                                           
-    # When I go to the profile                                    
-    # Then I check if the email exists                            
-    
-# Given the registered user is on the main page
-# When he moves on the the [a]-page
-# Then he is on the [a]-page
-# When he scrolls down to see the input fields
-# And types in his username
-# And types in his correct password and confirms the input via Enter
-# Then he is greeted
-# When he goes to his profile
-# Then he checks if there is an [b]
-
+    Given I am on the landing page
+    When I enter my correct credentials                                          
+    Then I am on my profile page                                           
 
 #Scenario: The registered user logs in
 #  Given Martin is on the login-page
--- cucumber_automation/step_definitions/LoginSteps.js
+++ cucumber_automation/step_definitions/LoginSteps.js
@@ -7,50 +7,20 @@ const { ScrollDown, user, correctPassword } = require('../page_objects/loginPage
 const { greetings, profile, profileEmailInput } = require('../page_objects/profilePage')
 const { username, password } = require('../config/index')
 
-Given('I am on the main page', { timeout: 2 * 5000 }, async function () {
+Given('I am on the landing page', { timeout: 6000 }, async function () {
   await openBrowser()
   await goToHomePage()
   await clickToLogin()
-})
-
-/*
-When('I click on the button for the Login-Page', async function () {
-  await goToHomePage()
-})
-
-Then('I get to the Login-Page', async function () {
-  await clickToLogin()
-})
-*/
-
-When('I scroll down', async function () {
   await ScrollDown()
+})
+
+When('I enter my correct credentials', async function () {
   await user(username)
   await correctPassword(password)
 })
 
-/*
-When('I fill in my username', async function () {
-  await user(username)
-})
-
-When('I type in the correct password and confirm via Enter', async function () {
-  await correctPassword(password)
-})
-*/
-
-Then('I am greeted', { timeout: 2 * 5000 }, async function () {
+Then('I am on my profile page', { timeout: 6000 }, async function () {
   await greetings()
   await profile()
   await profileEmailInput()
 })
-
-/*
-Then('I go to the profile', async function () {
-  await profile()
-})
-
-Then('I check if the email exists', async function () {
-  await profileEmailInput()
-})
-*/
